e54984a9a429da83fd9b7387e0358ae19a2e752a
Devin Lai <devin.lai@ichef.com.tw>
2022-07-15 09:51:28 +0800

    Add project files

---
+++ GoAnime/GoAnime/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GoAnime
+//
+//  Created by 賴柏宏 on 2022/7/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
