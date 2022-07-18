2c2233741d4d4a80d2a60ba8c05f56714250f3b5
Devin Lai <devin.lai@ichef.com.tw>
2022-07-18 14:27:18 +0800

    feat: separate configuration and view, add unit tests

--- GoAnime/GoAnime/AnimeView/AnimeItemConfiguration/AnimeItemContentView.swift
+++ GoAnime/GoAnime/AnimeView/AnimeItemConfiguration/AnimeItemContentView.swift
@@ -1,72 +1,13 @@
 //
-//  AnimeItemConfiguration.swift
+//  AnimeItemContentView.swift
 //  GoAnime
 //
 //  Created by 賴柏宏 on 2022/7/18.
 //
 
 import Foundation
-import UIKit
-import Utility
 import Combine
-
-protocol AnimeItemConfigurationDelegate: AnyObject {
-    func configuration(_ config: AnimeItemConfiguration, didTapFavorite model: AnimeItemModel)
-}
-
-final class AnimeItemConfiguration: UIContentConfiguration, Identifiable {
-    weak var delegate: AnimeItemConfigurationDelegate?
-    static let dateFormatter: DateFormatter = {
-        let df = DateFormatter()
-        df.dateFormat = "YYYY-MM-dd"
-        return df
-    }()
-    
-    let model: AnimeItemModel
-    
-    var id: String { model.id }
-    var imageUrl: String { model.imageUrl }
-    var title: String { model.title }
-    var type: String { model.type.rawValue }
-    var rank: Int { model.rank }
-    var fromDate: Date? { model.fromDate }
-    var toDate: Date? { model.toDate }
-    @Published var hideRank: Bool = false
-    @Published var isFavorite: Bool
-    
-    
-    init(model: AnimeItemModel) {
-        self.model = model
-        self.isFavorite = model.isFavorite
-    }
-    
-    func makeContentView() -> UIView & UIContentView {
-        AnimeItemContentView(
-            self)
-    }
-    
-    func updated(for state: UIConfigurationState) -> Self {
-        self
-    }
-    
-    func favoriteDidTapped() {
-        isFavorite = !isFavorite
-        delegate?.configuration(self, didTapFavorite: model)
-    }
-    
-    var dateText: String {
-        let from = Self.dateFormatter.animeString(from: fromDate)
-        let to = Self.dateFormatter.animeString(from: toDate)
-        return [from, to].joined(separator: " ~ ")
-    }
-}
-
-extension DateFormatter {
-    func animeString(from date: Date?) -> String {
-        guard let date = date else { return "-" }
-        return string(from: date)
-    }
-}
+import UIKit
 
 final class AnimeItemContentView: UIView, UIContentView {
     
@@ -207,14 +148,5 @@ final class AnimeItemContentView: UIView, UIContentView {
     @objc func addFavoriteDidTapped(_ sender: UIButton) {
         guard let config = configuration as? AnimeItemConfiguration else { return }
         config.favoriteDidTapped()
-    }
-}
-
-
-extension NSLayoutConstraint {
-    
-    func priority(_ priority: UILayoutPriority) -> Self {
-        self.priority = priority
-        return self
     }
 }
---
+++ GoAnime/GoAnimeTests/Anime/AnimeItemConfigurationTests.swift
@@ -0,0 +1,51 @@
+//
+//  AnimeItemConfigurationTests.swift
+//  GoAnimeTests
+//
+//  Created by 賴柏宏 on 2022/7/18.
+//
+
+import Foundation
+import XCTest
+@testable import GoAnime
+
+final class AnimeItemConfigurationTests: XCTestCase {
+    func test_configuration_data() {
+        let item = AnimeItemModel.mock
+        
+        let config = AnimeItemConfiguration(model: item)
+        XCTAssertEqual(config.id, item.id)
+        XCTAssertEqual(config.imageUrl, item.imageUrl)
+        XCTAssertEqual(config.title, item.title)
+        XCTAssertEqual(config.type, item.type.rawValue)
+        XCTAssertEqual(config.rank, item.rank)
+        XCTAssertEqual(config.fromDate, item.fromDate)
+        XCTAssertEqual(config.toDate, item.toDate)
+        XCTAssertEqual(config.isFavorite, item.isFavorite)
+        
+        XCTAssertEqual(config.dateText, "2022-07-18 ~ 2022-07-18")
+    }
+    
+    func test_configuraton_ViewType() {
+        let config = AnimeItemConfiguration(model: .mock)
+        XCTAssertTrue(config.makeContentView().isKind(of: AnimeItemContentView.self))
+    }
+    
+    func test_tap_favorite() {
+        let item = AnimeItemModel.mock
+        
+        let config = AnimeItemConfiguration(model: item)
+        
+        config.favoriteDidTapped()
+        
+        XCTAssertTrue(item.isFavorite)
+    }
+    
+    func test_Nil_Date() {
+        let item = AnimeItemModel.mock
+        item.toDate = nil
+        item.fromDate = nil
+        let config = AnimeItemConfiguration(model: item)
+        XCTAssertEqual(config.dateText, "- ~ -")
+    }
+}
